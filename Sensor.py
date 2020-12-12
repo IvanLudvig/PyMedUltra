ab1f3d6151c020c611963fd774ccde9581e48c3c
Квазаров <61160063+zugzvangg@users.noreply.github.com>
2020-12-13 00:13:50 +0300

    Чутка добавил

--- Sensor.py
+++ Sensor.py
@@ -1,2 +1,10 @@
+import json
 class Sensor:
-    pass
+    def __init__(self):
+        conf = 'res/config.json'
+        with open(conf) as jf:
+            configuration = json.load(jf)
+            self.SENSORS = configuration["Constants"]["SENSORS"]
+            self.DT_WIDTH = configuration["Constants"]["DT_WIDTH"]
+            self.DT_CARRYING = configuration["Constants"]["DT_CARRYING"]
+            
