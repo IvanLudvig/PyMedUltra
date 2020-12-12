0915837f8e60580ce9ef39367d597a4941e2e036
Квазаров <61160063+zugzvangg@users.noreply.github.com>
2020-12-12 16:30:06 +0300

    Переделал Dot, Vector2 в процессе

--- Dot.py
+++ Dot.py
@@ -1,3 +1,21 @@
+from Vector2 import Vector2
+
 class Dot:
-    pass
+    def __init__(self, pos, brightness=0):
+        self.pos = pos
+        self.brightness = brightness
+    
+    def getPos(self):
+        return self.pos
+
+    def getBrightness(self):
+        return self.brightness
+    
+    def setPos(self, pos):
+        self.pos = pos
+
+    def setBrightness(self, brightness):
+        self.brightness = brightness
+
+
 
--- Vector2.py
+++ Vector2.py
@@ -1,2 +1,34 @@
+import math
 class Vector2:
-    pass
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def setX(self, x:float):
+        self.x = x
+
+    def setY(self, y:float):
+        self.y = y
+
+    def getX(self):
+        return self.x 
+
+    def getY(self):
+        return self.y
+    
+    def scalar(self, A:Vector2, B:Vector2) -> float:
+        return A.getX() * B.getX() + A.getY() * B.getY()
+    
+    def length(self, A:Vector2, B:Vector2) -> float:
+        tmp = Vector2((A.getX()-B.getX()), (A.getY()-B.getY()))
+        return math.sqrt(self.scalar(tmp, tmp))
+    
+    def area(self, A:Vector2, B:Vector2, C:Vector2)->float:
+        return (B.getX() - A.getX()) * (C.getY() - A.getY()) - (B.getY() - A.getY()) * (C.getX() - A.getX())
+    
+    def doIntersact(self, A:Vector2, B:Vector2, C:Vector2, D:Vector2, Result:Vector2)->bool:
+        if  not (self.area(A, D, B) * self.area(A, B, C) > 0 and self.area(D, B, C) * self.area(D, C, A) > 0):
+            return False
+
+
+
