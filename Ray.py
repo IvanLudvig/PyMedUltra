0d6134c81c47a955e418440d9ecf3cb4fcd3f7d9
Квазаров <61160063+zugzvangg@users.noreply.github.com>
2020-12-12 18:38:41 +0300

    Начал Ray, чуток пофиксил конфиг
    Там поле было непонятно какое, мб кто-то потерял

--- Ray.py
+++ Ray.py
@@ -1,2 +1,25 @@
+import json
+from Vector2 import Vector2
 class Ray:
-    pass
+    def __init__(self,pos:Vector2=Vector2(),
+		   velocity:Vector2=Vector2(),
+		   material:int,
+		   intensity:float,
+		   nextEncounter:float,
+		   obstacle_number:int,
+		   vertice_number:int,
+		   left:Ray,
+		   right:Ray,
+		   kill_marked:invalid,
+		   const std::vector<Node *> &virtual_neighbors_left,
+		   const std::vector<Node *> &virtual_neighbors_right):
+        conf = 'res/config.json'
+        with open(conf) as jf:
+            configuration = json.load(jf)
+            self.VISIBILITY_THRESHOLD = configuration["Constants"]["VISIBILITY_THRESHOLD"]
+            self.SENSORS = configuration["Constants"]["SENSORS"]
+        self.pos = Vector2()
+        self.velocity = Vector2()
+
+
+            
