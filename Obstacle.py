129ab9d80c999232526dcce630c2d2704da2ec5b
Квазаров <61160063+zugzvangg@users.noreply.github.com>
2020-12-12 18:07:09 +0300

    Obstacle
    Здесь сделал pos не на numpy массиве, так как в принципе в этот массив добавляется мало элементов (4, судя по конфигу и начальному файлу) и мы сильно не выиграем по скорости

--- Obstacle.py
+++ Obstacle.py
@@ -1,2 +1,29 @@
+import json
+import numpy as np
+from Vector2 import Vector2
 class Obstacle:
-    pass
+    def __init__(self, x=0, y=0):
+        conf = 'res/config.json'
+        with open(conf) as jf:
+            configuration = json.load(jf)
+            self.relativeSpeed = configuration['OBSTACLES']['RELATIVE_SPEED']
+        self.pos = []
+            
+    def getPos(self, i:int)->Vector2:
+        return self.pos[i]
+
+    def setPos(self, j:int, pos:Vector2)->None:
+        self.pos[j]=pos
+
+    def getCRel(self)->float:
+        return self.relativeSpeed
+    
+    def setCRel(self, relativeSpeed:float)->None:
+        self.relativeSpeed = relativeSpeed
+
+    def addPos(self, pos:Vector2)->None:
+        self.pos.append(pos)
+
+    
+
+
